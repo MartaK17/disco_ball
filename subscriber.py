91074dbd193241f7c6d3dbed9f053b0692ea6203
ubuntu22 <martakwiecinska17@gmail.com>
2023-06-06 19:13:23 +0200

    open and close file in chrome

--- subscriber.py
+++ subscriber.py
@@ -1,6 +1,22 @@
 import paho.mqtt.client as mqtt
 import json
 import webbrowser  
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+
+driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver")
+# Store the ID of the original window
+original_window = driver.current_window_handle
+#switch to tab browser
+#launch URL
+#driver.get("https://accounts.google.com/")
+# Setup wait for later
+#wait = WebDriverWait(driver, 10)
+# Store the ID of the original window
+#original_window = driver.current_window_handle
+#switch to tab browser
+#driver.switch_to.new_window('tab')
+
 
 # The callback function of connection
 
@@ -13,13 +29,26 @@ def on_message(client, userdata, msg):
     json_string = str(msg.payload.decode("utf-8","ignore"))
     dictionary = json.loads(json_string)
     if dictionary["action"] == "brightness_move_up": 
+
+
+        driver.switch_to.new_window('tab')
+        # Setup wait for later
+        wait = WebDriverWait(driver, 500)
         url= 'https://www.youtube.com/watch?v=TU7V5_Jy-RI'  
         # Open the URL using open() function of module  
-        webbrowser.open_new_tab(url)  
+        driver.get(url)
+        #c = driver.window_handles[1]
+        #driver.switch_to.window(c)
         state = "ON"
         client.publish('zigbee2mqtt/0x385b44fffe164f57/set', payload=state, qos=0, retain=False)
     elif dictionary["action"] == "on":
         state = "OFF"
+        #c = driver.window_handles[1]
+        #driver.switch_to.window(c)
+        driver.close()
+        driver.switch_to.window(original_window)
+        #switch to tab browser
+        #driver.switch_to.new_window('tab')
         client.publish('zigbee2mqtt/0x385b44fffe164f57/set', payload=state, qos=0, retain=False)
 def subscribe():
     client = mqtt.Client()
